b3d089f6b4a424422bb809969249b17213aaff9c
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2022-02-20 05:28:00 +0100

    Update 2.0
    Added Logging
    Added Reset all

--- modules/leveling.py
+++ modules/leveling.py
@@ -79,3 +79,48 @@ def resetxp(member_id):
         print("Done")
     except KeyError:
         print(f"{member_id} not Registered!")
+
+def reset(member_id):
+    try:
+        with open("data/xp.json", "r") as d:
+            xp = json.load(d)
+            xp[str(f"{member_id}")] = "1"
+        with open("data/xp.json", "w") as d:
+            json.dump(xp, d, indent=4)
+        print("Done")
+        with open("data/level.json", "r") as d:
+            xp = json.load(d)
+            xp[str(f"{member_id}")] = "1"
+        with open("data/level.json", "w") as d:
+            json.dump(xp, d, indent=4)
+        print("Done")
+    except KeyError:
+        print(f"{member_id} not Registered!")
+
+
+def log_register(member_id):
+    log = f"{member_id.mention} is now Registered!"
+    return(log)
+
+def log_addlvl(member_id):
+    log = f"{member_id.mention} has Leveled UP"
+    return(log)
+
+def log_addxp(member_id):
+    log = f"{member_id.mention} has gained XP"
+    return(log)
+
+def log_getlvl(member_id):
+    log = f"{member_id.mention} Gettign LVL from JSON!"
+    return(log)
+
+def log_getxp(member_id):
+    log = f"{member_id.mention} Getting XP from JSON!"
+    return(log)
+
+def log_resetxp(member_id):
+    log = f"{member_id.mention} XP Reset!"
+    return(log)
+
+def log_reset(member_id):
+    log = f"{member_id} Reset ALL!"
